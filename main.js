42209438610169fde9ef78f6f0aa7ee614a38413
ignacio vilte <vilteignacio10@gmail.com>
2023-07-03 23:42:15 -0300

    pre1

---
+++ main.js
@@ -0,0 +1,34 @@
+//Funciones
+/* let vida = 100;
+
+const recibirDaño = (daño) => {
+vida -= daño;
+if(vida < 0){
+    vida = 0;
+}
+}
+recibirDaño(120)
+ */
+
+//                  Destructuracion de objetos
+
+/* const usuario1 = {
+    full_name:"Ignacio",
+    edad: 20,
+    cursos: {
+        primer:"DW",
+        segundo:"JS",
+        tercero:"RJS"
+    }
+}
+ */
+/* const { nombre, edad, cursos:{segundo} } = usuario1;
+console.log(nombre,edad,segundo); */
+//                 ALIAS
+/* const{full_name:nombre} = usuario1;
+console.log(nombre); */
+
+//                 OPERADOR TERNARIO
+/* temperatura = 21
+mensaje = temperatura <= 20 ? console.log("dia frio") : console.log("dia caluroso"); */
+
